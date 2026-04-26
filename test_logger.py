f608ca00f2f4e0b9908b754c8124274b10cf9865
Rocky0Shao <rockyshao22@gmail.com>
2026-04-26 14:42:41 -0400

    merge hmi logger to main

--- test_logger.py
+++
@@ -1,8 +0,0 @@
-import HMI_LOGGER
-
-logger = HMI_LOGGER.HMI_LOGGER()
-while True:
-    user_input = input("enter message to log (type 'exit' to quit): ")
-    if user_input == "exit":
-        break
-    logger.log(user_input)
